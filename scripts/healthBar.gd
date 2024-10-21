d179e9dc6531c53d4d93003e2de44d34910df680
Felipe Takeshi <167320163+Takexin@users.noreply.github.com>
2024-10-21 13:34:33 -0300

    Update healthBar.gd

--- scripts/healthBar.gd
+++ scripts/healthBar.gd
@@ -2,7 +2,7 @@ extends ProgressBar
 
 onready var player = get_parent().get_node("character")
 func _ready():
-	CanvasLayer.position
+	pass
 func process():
 	if player:
 		self.value = player.health
